6f98815a5f4e7b84f39c5804d5a6018d10f50823
Schneison <larson.schneider11@gmail.com>
2022-11-18 17:07:13 +0100

    Add builder and documentation to the Mesh.cs

--- TuringMachine/Data/Mesh.cs
+++ TuringMachine/Data/Mesh.cs
@@ -2,40 +2,144 @@
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Runtime.CompilerServices;
+using System.Windows;
 
 namespace TuringMachine.Data;
 
+/// <summary>
+/// Network of transitions for one state of the turing machine.
+/// Main use is to move from one state to the other.
+/// </summary>
 public class Mesh
 {
-	private readonly  ImmutableDictionary<ITuple, Transition> _transitions;
+	/// <summary>
+	/// All transitions of this mesh
+	/// </summary>
+	private readonly ImmutableDictionary<ITuple, Transition> _transitions;
 
-	public Mesh(State state, IEnumerable<Transition> transitions)
+	private Mesh(State state, int tapeCount, ImmutableDictionary<ITuple, Transition> transitions)
 	{
 		State = state;
-		_transitions = BuildTransitions(transitions);
+		TapeCount = tapeCount;
+		_transitions = transitions;
 	}
 
-	private static ImmutableDictionary<ITuple, Transition> BuildTransitions(IEnumerable<Transition> transitions)
+	/// <summary>
+	/// Retrieves valid transition for the specific tuple and throws exception if none was found.
+	/// <p/>
+	/// Please check <see cref="Accepts"/> if you don't know if the tuple is valid.
+	/// </summary>
+	/// <param name="tuple">Valid input tuple for the mesh</param>
+	public Transition this[ITuple tuple] => _transitions[tuple];
+
+	/// <summary>
+	/// Checks if the given tuple is a valid input for this mesh.
+	/// </summary>
+	/// <param name="tuple">Possible input</param>
+	/// <returns>True if the tuple is valid.</returns>
+	public bool Accepts(ITuple tuple)
 	{
-		var trxBuilder = ImmutableDictionary.CreateBuilder<ITuple, Transition>();
-		foreach (var trx in transitions)
-		{
-			foreach (var comb in trx.GetInputCombinations())
-			{
-				trxBuilder[comb] = trx;
-			}
-		}
-
-		return trxBuilder.ToImmutable();
+		return tuple.Length == TapeCount && _transitions.ContainsKey(tuple);
 	}
 
-	public State State
-	{
-		get;
-	}
-	
+	/// <summary>
+	/// State of the turing machine that the mesh belongs to.
+	/// </summary>
+	public State State { get; }
+
+	/// <summary>
+	/// Amount of tapes that this mesh accepts.
+	/// Every transition has to have this amount of mutations defined.
+	/// </summary>
+	public int TapeCount { get; }
+
+	/// <summary>
+	/// Returns a transition for the given input array of symbols.
+	/// <p/>
+	/// Used by the turing machine to transition from one state to the other if all tapes contain the valid symbol for
+	/// the equivalent mutation in the transition.
+	/// </summary>
+	/// <param name="symbols">Input array</param>
+	/// <returns>Transition that contains the valid mutation for the equivalent symbol in the input array.</returns>
 	public Transition Find(ISymbol[] symbols)
 	{
 		return Transition.None;
+	}
+
+	/// <summary>
+	/// Builder that is the only way to create a mesh.
+	/// </summary>
+	public class Builder
+	{
+		private readonly HashSet<Transition> _transitions = new();
+		private int _tapeCount = 1;
+		private State _state = State.Empty;
+
+		public Builder WithTransition(Transition transition)
+		{
+			_transitions.Add(transition);
+			return this;
+		}
+
+		/// <summary>
+		/// Defines the amount of tapes that this mesh accepts.
+		/// Every transition has to have this amount of mutations defined.
+		/// </summary>
+		/// <param name="tapeCount">Amount of tapes</param>
+		/// <returns>The builder instance for builder chaining.</returns>
+		public Builder FromTape(int tapeCount)
+		{
+			this._tapeCount = tapeCount;
+			return this;
+		}
+
+		/// <summary>
+		/// Defines the state of the turing machine that the mesh belongs to.
+		/// </summary>
+		/// <param name="state">A state of a turing machine</param>
+		/// <returns>The builder instance for builder chaining.</returns>
+		public Builder FromState(State state)
+		{
+			this._state = _state;
+			return this;
+		}
+
+		/// <summary>
+		/// Creates transition dictionary that contains a key value pair for every possible input combination of
+		/// all transitions to there transition.
+		/// <p/>
+		/// Additionally checks if every transition has the right amount of mutations which has to equal the tape count.
+		/// </summary>
+		/// <returns>Dictionary that contains a key value pair for every possible input combination of
+		/// all transitions to there transition.</returns>
+		/// <exception cref="NotSupportedException"></exception>
+		private ImmutableDictionary<ITuple, Transition> BuildTransitions()
+		{
+			var trxBuilder = ImmutableDictionary.CreateBuilder<ITuple, Transition>();
+			foreach (var trx in _transitions)
+			{
+				foreach (var comb in trx.GetInputCombinations())
+				{
+					if (comb.Length != _tapeCount)
+					{
+						throw new NotSupportedException(
+							$"Mutation amount of a transition does not equal the amount of tapes {_tapeCount}: {trx}");
+					}
+
+					trxBuilder[comb] = trx;
+				}
+			}
+
+			return trxBuilder.ToImmutable();
+		}
+
+		/// <summary>
+		/// Creates a mesh from the content of this builder which was verified.
+		/// </summary>
+		/// <returns>Newly created valid mesh</returns>
+		public Mesh Create()
+		{
+			return new Mesh(_state, _tapeCount, BuildTransitions());
+		}
 	}
 }
