b354f092d9596a6b84bc03e1005d76b57517ceb9
Schneison <larson.schneider11@gmail.com>
2022-11-18 17:29:41 +0100

    Add comments to Mutation.cs and SymbolMutation.cs

--- TuringMachine/Data/SymbolMutation.cs
+++ TuringMachine/Data/SymbolMutation.cs
@@ -2,9 +2,18 @@
 
 namespace TuringMachine.Data;
 
+/// <summary>
+/// Mutation based on one input and one output symbol
+/// </summary>
 public class SymbolMutation : Mutation
 {
+	/// <summary>
+	/// Valid symbol on the tape
+	/// </summary>
 	private readonly ISymbol _input;
+	/// <summary>
+	/// Written symbol to the tape
+	/// </summary>
 	private readonly ISymbol _output;
 
 	public SymbolMutation(ISymbol input, ISymbol output, Direction dir) : base(dir)
@@ -13,6 +22,12 @@ public class SymbolMutation : Mutation
 		_output = output;
 	}
 
+	/// <summary>
+	/// Retrieves the symbol that should be placed onto the tape if this mutation input matches the symbol on the
+	/// corresponding tape.
+	/// </summary>
+	/// <param name="input">Symbol on the tape that matched <see cref="Matches"/> of this mutation</param>
+	/// <returns>Symbol for the tape</returns>
 	public override ISymbol ToTape(ISymbol input)
 	{
 		return _output;
