be913a85f87fa8745b31f18a6ddd1e53277b74fb
Schneison <larson.schneider11@gmail.com>
2022-11-18 18:14:26 +0100

    Improve test for Mesh.cs

--- TuringMachine.UnitTests/Data/MeshTest.cs
+++ TuringMachine.UnitTests/Data/MeshTest.cs
@@ -16,30 +16,44 @@ public class MeshTest
 		});
 		Assert.Catch<NotSupportedException>(() =>
 		{
-			builder.FromTape(1).WithTransition(TrxFromPairs((SymbolA, SymbolA),(SymbolA, SymbolA))).Create();
+			builder.FromTape(1).WithTransition(TrxFromPairs((SymbolA, SymbolA), (SymbolA, SymbolA))).Create();
 		});
 	}
 
 	[Test]
 	public void Accepts()
-    {
-        var mesh = Mesh.CreateBuilder()
-	        .WithTransition(TrxFrom(SymbolA, SymbolA)).Create();
-        var other = Mesh.CreateBuilder()
-	        .FromTape(2).WithTransition(TrxFromPairs((SymbolA, SymbolA),(SymbolA, SymbolA))).Create();
-        Assert.Multiple(() =>
-        {
-            Assert.That(mesh.Accepts(Tuple.Create(SymbolA)), Is.True);
-            Assert.That(mesh.Accepts(Tuple.Create(SymbolB)), Is.False);
-            Assert.That(other.Accepts(Tuple.Create(SymbolA)), Is.False);
-            Assert.That(other.Accepts(Tuple.Create(SymbolA, SymbolA)), Is.True);
-            Assert.That(other.Accepts(Tuple.Create(SymbolB, SymbolA)), Is.False);
-        });
-    }
+	{
+		var mesh = Mesh.CreateBuilder()
+			.WithTransition(TrxFrom(SymbolA, SymbolA)).Create();
+		var other = Mesh.CreateBuilder()
+			.FromTape(2).WithTransition(TrxFromPairs((SymbolA, SymbolA), (SymbolA, SymbolA))).Create();
+		Assert.Multiple(() =>
+		{
+			Assert.That(mesh.Accepts(Tuple.Create(SymbolA)), Is.True);
+			Assert.That(mesh.Accepts(Tuple.Create(SymbolB)), Is.False);
+			Assert.That(other.Accepts(Tuple.Create(SymbolA)), Is.False);
+			Assert.That(other.Accepts(Tuple.Create(SymbolA, SymbolA)), Is.True);
+			Assert.That(other.Accepts(Tuple.Create(SymbolB, SymbolA)), Is.False);
+		});
+	}
 
-    [Test]
+	[Test]
+	public void Index()
+	{
+		var mesh = Mesh.CreateBuilder()
+			.WithTransition(TrxFrom(SymbolA, SymbolA)).Create();
+		Assert.Multiple(() =>
+		{
+			Assert.Catch<KeyNotFoundException>(() => { Assert.That(mesh[(SymbolA, SymbolA)], Is.Not.Null); });
+			Assert.That(mesh[Tuple.Create(SymbolA)], Is.Not.Null);
+		});
+	}
+
+	[Test]
 	public void Find()
 	{
-		
+		var mesh = Mesh.CreateBuilder()
+			.WithTransition(TrxFrom(SymbolA, SymbolA)).Create();
+		mesh.Find(Array.Empty<ISymbol>());
 	}
 }
