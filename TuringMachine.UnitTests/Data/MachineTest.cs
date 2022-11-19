1f295e0c887981c4a31835301d2bf2dd900c3e17
Schneison <larson.schneider11@gmail.com>
2022-11-19 17:13:52 +0100

    Add tests and comments for Machine.cs

---
+++ TuringMachine.UnitTests/Data/MachineTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using TuringMachine.Data;
+
+namespace TuringMachine.UnitTests.Data;
+
+[TestFixture]
+public class MachineTest {
+	[Test]
+	public void Test() {
+		var design = new Design(1);
+		var machine = new Machine(design);
+		machine.TryApplyTransition();
+	}
+}
--- TuringMachine/Data/Machine.cs
+++ TuringMachine/Data/Machine.cs
@@ -2,10 +2,23 @@ using System.Collections.Generic;
 
 namespace TuringMachine.Data;
 
+/// <summary>
+/// Main class for the turing machine simulation.
+/// </summary>
 public class Machine {
+	/// <summary>
+	/// Meshes for every state.
+	/// </summary>
 	private readonly Dictionary<State, Mesh> _meshes;
+	/// <summary>
+	/// All tapes of the machine.
+	/// </summary>
 	private readonly Tape[] _tapes;
 
+	/// <summary>
+	/// Creates a new machine for the given design.
+	/// </summary>
+	/// <param name="design">Config of the machine.</param>
 	public Machine(Design design) {
 		CurrentState = State.Empty;
 		_tapes = new Tape[design.TapeCount];
@@ -14,8 +27,15 @@ public class Machine {
 		}
 	}
 
-	public State CurrentState { get; internal set; }
+	/// <summary>
+	/// Current state of the machine.
+	/// </summary>
+	public State CurrentState { get; }
 
+	/// <summary>
+	/// Tries to execute the next step of the machine.
+	/// </summary>
 	public void TryApplyTransition() {
+		// Current empty
 	}
 }
