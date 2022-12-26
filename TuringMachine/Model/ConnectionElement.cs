79245806ef7e05f5487e6193a0d3a60bafd68746
Schneison <larson.schneider11@gmail.com>
2022-12-26 01:39:30 +0100

    Fix transition label state

--- TuringMachine/Model/ConnectionElement.cs
+++ TuringMachine/Model/ConnectionElement.cs
@@ -77,61 +77,12 @@ public class ConnectionElement : GraphElement {
 		set => SetProperty(ref _end, value);
 	}
 
-	/// <summary>
-	/// Get the required height and width of the specified text. Uses FortammedText
-	/// </summary>
-	public static Size MeasureTextSize(string text, FontFamily fontFamily, FontStyle fontStyle, FontWeight fontWeight, FontStretch fontStretch, double fontSize) {
-		var ft = new FormattedText(text,
-			CultureInfo.CurrentCulture,
-			FlowDirection.LeftToRight,
-			new Typeface(this.textBlock.FontFamily, this.textBlock.FontStyle, this.textBlock.FontWeight, this.textBlock.FontStretch),
-			fontSize,
-			Brushes.Black, VisualTreeHelper.GetDpi(this.textBlock).PixelsPerDip);
-		return new Size(ft.Width, ft.Height);
-	}
-
-	/// <summary>
-	/// Get the required height and width of the specified text. Uses Glyph's
-	/// </summary>
-	public static Size MeasureText(string text, FontFamily fontFamily, FontStyle fontStyle, FontWeight fontWeight, FontStretch fontStretch, double fontSize) {
-		var typeface = new Typeface(fontFamily, fontStyle, fontWeight, fontStretch);
-
-		if (!typeface.TryGetGlyphTypeface(out var glyphTypeface)) {
-			return MeasureTextSize(text, fontFamily, fontStyle, fontWeight, fontStretch, fontSize);
-		}
-
-		double totalWidth = 0;
-		double height = 0;
-
-		foreach (var textChar in text) {
-			var glyphIndex = glyphTypeface.CharacterToGlyphMap[textChar];
-
-			var width = glyphTypeface.AdvanceWidths[glyphIndex] * fontSize;
-
-			var glyphHeight = glyphTypeface.AdvanceHeights[glyphIndex] * fontSize;
-
-			if (glyphHeight > height) {
-				height = glyphHeight;
-			}
-
-			totalWidth += width;
-		}
-
-		return new Size(totalWidth, height);
-	}
-
-	"a | b | 0"
-
 	public Vector2 TextOverflow() {
-		if (IsSelfLoop) {
-			if (_alternativeDirection) {
-				return new Vector2(0, StateElement.StateRadius * 2);
-			}
-
-			return new Vector2(0, -StateElement.StateRadius * 2);
-		}
-
-		return Perpendicular * 0;
+		return IsSelfLoop switch {
+			true when _alternativeDirection => new Vector2(0, StateElement.StateRadius * 2),
+			true => new Vector2(0, -StateElement.StateRadius * 2),
+			_ => Perpendicular * 0
+		};
 	}
 
 	public void OnRemoveNode() {
--- TuringMachine/View/TransitionLabel.xaml.cs
+++ TuringMachine/View/TransitionLabel.xaml.cs
@@ -1,28 +1,67 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Globalization;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace TuringMachine.View {
 	/// <summary>
 	/// Interaction logic for TransitionLabel.xaml
 	/// </summary>
 	public partial class TransitionLabel : UserControl {
-		private TextBlock _textBlock;
+		private readonly TextBlock _textBlock;
+
 		public TransitionLabel() {
 			InitializeComponent();
 			DataContext = this;
 			_textBlock = (TextBlock) FindName("label");
+		}
+
+		private Typeface CreateTypeface() {
+			return new Typeface(_textBlock.FontFamily, _textBlock.FontStyle, _textBlock.FontWeight,
+				_textBlock.FontStretch);
+		}
+
+		/// <summary>
+		/// Get the required height and width of the specified text. Uses FortammedText
+		/// </summary>
+		public Size MeasureTextSize(string text, double fontSize) {
+			var ft = new FormattedText(text,
+				CultureInfo.CurrentCulture,
+				FlowDirection.LeftToRight,
+				CreateTypeface(),
+				fontSize,
+				Brushes.Black, VisualTreeHelper.GetDpi(_textBlock).PixelsPerDip);
+			return new Size(ft.Width, ft.Height);
+		}
+
+		/// <summary>
+		/// Get the required height and width of the specified text. Uses Glyph's
+		/// </summary>
+		public Size MeasureText(string text, double fontSize) {
+			var typeface = CreateTypeface();
+
+			if (!typeface.TryGetGlyphTypeface(out var glyphTypeface)) {
+				return MeasureTextSize(text, fontSize);
+			}
+
+			double totalWidth = 0;
+			double height = 0;
+
+			foreach (var textChar in text) {
+				var glyphIndex = glyphTypeface.CharacterToGlyphMap[textChar];
+
+				var width = glyphTypeface.AdvanceWidths[glyphIndex] * fontSize;
+
+				var glyphHeight = glyphTypeface.AdvanceHeights[glyphIndex] * fontSize;
+
+				if (glyphHeight > height) {
+					height = glyphHeight;
+				}
+
+				totalWidth += width;
+			}
+
+			return new Size(totalWidth, height);
 		}
 	}
 }
